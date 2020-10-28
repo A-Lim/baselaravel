9959bc1336bc2c63975d64d06aa9adebab4932cf
Alexius Lim <alexiuslim@Alexiuss-MacBook-Pro.local>
2020-10-28 23:41:38 +0800

    initial commit

---
+++ app/Repositories/RepositoryServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Repositories;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider {
+
+    public function register() {
+        $this->app->bind('App\Repositories\Auth\OAuthRepositoryInterface', 'App\Repositories\Auth\OAuthRepository');
+        $this->app->bind('App\Repositories\User\UserRepositoryInterface', 'App\Repositories\User\UserRepository');
+        $this->app->bind('App\Repositories\UserGroup\UserGroupRepositoryInterface', 'App\Repositories\UserGroup\UserGroupRepository');
+        $this->app->bind('App\Repositories\SystemSetting\SystemSettingRepositoryInterface', 'App\Repositories\SystemSetting\SystemSettingRepository');
+        $this->app->bind('App\Repositories\Permission\IPermissionRepository', 'App\Repositories\Permission\PermissionRepository');
+    }
+}
